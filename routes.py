07f84ef1949c3c1bd708c5173d7e3085dfa4e28e
Xiao Guo <saber.guo@gmail.com>
2016-04-14 21:33:02 +0800

    setting

--- routes.py
+++ routes.py
@@ -8,6 +8,7 @@ handlers = [
     (r"/apply_adding", apply_adding_handler),
     (r"/reboot", reboot_handler),
     (r"/device_time", device_time_handler),
+    (r"/device_location", device_location_handler),
     (r"/time_synchronize", time_synchronize_handler),
     (r"/operater_position", operator_position_handler),
 ]
